aa1e3d680f572104de1beee7926e59c5d7aba892
Eric <erictianc@outlook.com>
2021-02-12 19:53:56 +0800

    github相关放入about_github.go

---
+++ about_github.go
@@ -0,0 +1,38 @@
+//有关github的所有函数
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const GET_VERSION = "https://api.github.com/repos/%s/releases/latest"
+
+func get_latest_version_url(repo string, index int) (string, error) {
+	/* 获取仓库最新Release的下载url
+	 *
+	 * 参数：
+	 * 	repo: 仓库地址，例："EricTianC/GraiaOK"
+	 *	index: 下载文件的下标，下载第一个就是0
+	 * 返回:
+	 *	最新仓库地址  string
+	 *	异常  error
+	 */
+	resp, err := http.Get(fmt.Sprintf(GET_VERSION, repo))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("请求失败：%s", resp.Status)
+	}
+
+	data := make(map[string]interface{})
+	json.NewDecoder(resp.Body).Decode(&data)
+	log.Printf("target version: %s    %s\n%s\n", data["tag_name"], data["published_at"], data["body"])
+	download_url := data["assets"].([]interface{})[index].(map[string]interface{})["browser_download_url"].(string)
+	return download_url, nil
+}
--- get_mcl.go
+++ get_mcl.go
@@ -1,54 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	//"log"
 	"fmt"
-	"log"
-	"net/http"
-	"net/url"
-	"path"
-	"runtime"
 )
 
-const (
-	GETVERSION = "https://api.github.com/repos/%s/releases/latest"
-	REPOURL    = "iTXTech/mcl-installer"
-)
-
-var INDEXMAP = map[string]int{
-	"linux":   0,
-	"macos":   1,
-	"windows": 2,
-}
-
-func get_url() (string, error) {
-	resp, err := http.Get(fmt.Sprintf(GETVERSION, REPOURL))
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("请求失败：%s", resp.Status)
-	}
-	data := make(map[string]interface{})
-	json.NewDecoder(resp.Body).Decode(&data)
-	log.Printf("target version: %s\t%s\n", data["tag_name"], data["published_at"])
-	log.Println(data["body"])
-	download_url := data["assets"].([]interface{})[INDEXMAP[runtime.GOOS]].(map[string]interface{})["browser_download_url"].(string)
-	return download_url, nil
-}
+const REPOURL = "iTXTech/mirai-console-loader"
 
 func get_mcl() {
-	downUrl, err := get_url()
-	u, err := url.ParseRequestURI(downUrl)
-	filename := path.Base(u.Path)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	if !save(downFile(downUrl), filename) {
-		log.Fatal("保存失败")
-		return
-	}
+	downUrl, err := get_latest_version_url(REPOURL, 0)
+	fmt.Println(downUrl)
+	fmt.Println(err.Error())
 }
