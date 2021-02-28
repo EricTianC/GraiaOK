0750d772aae087ea2dde233b01e8785ec4d3455f
Eric Tian <erictianc@outlook.com>
2021-02-28 14:27:08 +0800

    graiax_cli!!!

--- environment/graia.go
+++ environment/graia.go
@@ -8,7 +8,7 @@ import (
 func CheckGraia() {
 	_, err := exec.LookPath("graiax")
 	if err != nil {
-		cmd := exec.Command("pip", "install", "-U", "graiax")
+		cmd := exec.Command("pip", "install", "-U", "graiax_cli")
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
